4c2a1087a5e2f31c509e79af789b12f2ff668f93
VAN1LLA <tlink1776@gmail.com>
2023-02-23 18:27:54 -0500

    Final commit

---
+++ Model/CountryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp.Model
+{
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public bool Active { get; set; }
+    }
+}
